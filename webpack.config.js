a56fe0486d7eaa500c3e73ea84bd8bddd99d856a
Krystal <momogugu1107@gmail.com>
2016-11-02 21:41:28 +0800

    webpack自动更新

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,17 @@
 var path = require('path');
 var webpack = require('webpack');
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');
 
 module.exports = {
   entry: [
-    // 'webpack/hot/dev-server',
-    // 'webpack-dev-server/client?http://localhost:8080',
-    './src/show.js'
+    'webpack/hot/dev-server',
+    'webpack-dev-server/client?http://localhost:8080',
+    path.resolve(__dirname, 'src/show.js')
   ],
   output: {
     path: path.join(__dirname, 'build'),
-    filename: "js/bundle.js",
-    publicPath: "build/"
+    filename: "bundle.js",
+    publicPath: "/js/"
   },
   resolve: {
     extensions: ['', '.js', '.jsx'],
@@ -29,8 +30,17 @@ module.exports = {
           }
       }
     ]
-  }
-  // plugins: [
-  //   new webpack.HotModuleReplacementPlugin()
-  // ]
+  },
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true,
+    contentBase: './build',
+    port: 8080
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new OpenBrowserPlugin({ url: 'http://localhost:8080' })
+  ]
 }
