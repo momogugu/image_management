49f391ff18ae25e760d30a0f03d7d758be4e5791
Krystal <momogugu1107@gmail.com>
2016-11-03 10:55:02 +0800

    点击分类切换图片

--- src/components/Album/Album.jsx
+++ src/components/Album/Album.jsx
@@ -3,7 +3,45 @@ import React from 'react';
 import Images from '../Images/Images.jsx';
 
 class Album extends React.Component {
+	appendImage(num) {
+		var _this = this;
+		var offset = 0;
+		var imageArr = _this.props.imageArr;
+		$(".col-1, .col-2, .col-3").empty();
+		if (num) {
+	        while (num--) {
+	            (function(i) {
+	                if (imageArr[offset]) {
+	                    var tmpImage = new Image();
+	                    tmpImage.src = imageArr[offset];
+	                    tmpImage.className = "min";
+	                    console.log(tmpImage)
+	                    $(tmpImage).load(function() {
+	                        var col = _this.getShortCol();
+	                        col.append(tmpImage);
+	                    });
+	                    offset++;
+	                }
+	            }(num))
+	        }
+	    }
+	}
+	getShortCol() {
+	    var height1 = $(".col-1").height();
+	    var height2 = $(".col-2").height();
+	    var height3 = $(".col-3").height();
+
+	    if (height1 <= height2 && height1 <= height3) {
+	        return $(".col-1");
+	    } else if (height2 <= height1 && height2 <= height3) {
+	        return $(".col-2");
+	    } else {
+	        return $(".col-3");
+	    }
+	}
 	render() {
+		console.log(this.props.imageArr);
+		this.appendImage(6);
 		return (
 			<div className="water-fall">
 				<div className="col-img">
--- src/components/Manager/Manager.jsx
+++ src/components/Manager/Manager.jsx
@@ -24,51 +24,14 @@ class Manager extends React.Component {
 					image: response,
 					imageArr: response.allimage
 				});
-            	 _this.appendImage(6);
+            	 // _this.appendImage(6);
 			}
 		});
-	}
-	appendImage(num) {
-		var _this = this;
-		var offset = 0;
-		var imageArr = _this.state.imageArr;
-		if (num) {
-	        while (num--) {
-	            (function(i) {
-	                if (imageArr[offset]) {
-	                    var tmpImage = new Image();
-	                    tmpImage.src = imageArr[offset];
-	                    tmpImage.className = "min";
-	                    console.log(tmpImage);
-	                    $(tmpImage).load(function() {
-	                        var col = _this.getShortCol();
-	                        col.append(tmpImage);
-	                    });
-	                    offset++;
-	                }
-	            }(num))
-	        }
-	    }
-	}
-	getShortCol() {
-	    var height1 = $(".col-1").height();
-	    var height2 = $(".col-2").height();
-	    var height3 = $(".col-3").height();
-
-	    if (height1 <= height2 && height1 <= height3) {
-	        return $(".col-1");
-	    } else if (height2 <= height1 && height2 <= height3) {
-	        return $(".col-2");
-	    } else {
-	        return $(".col-3");
-	    }
 	}
 	changeClass(className) {
 		this.setState({
 			imageArr: this.state.image[className]
 		});
-		var imageArr = this.state.imageArr;
-		this.appendImage(6);
 	}
 	render() {
 		return (
